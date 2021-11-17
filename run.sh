f6708b635c3cbd79708bc3cb9f51ded3e7e08e68
Vladislav <bshkrvv@gmail.com>
2021-11-17 10:18:15 +0300

    Configure flathub repository

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ sudo dnf --assumeyes --refresh upgrade
 # Enable additional repositories
 sudo dnf --assumeyes install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf --assumeyes install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Remove packages
 pkglist=$(<remove.list)
